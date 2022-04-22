2dc875558f83f558544dc6bb1406d1490bc58466
Ronaldo Alves de Andrade <ronaldo.andrade266@gmail.com>
2022-04-22 17:05:00 -0300

    Creates expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_182649) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_200334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.decimal "value", precision: 20, scale: 2, null: false
+    t.string "description", limit: 100, null: false
+    t.date "due_at", null: false
+    t.date "received_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incomes", force: :cascade do |t|
     t.decimal "value", precision: 20, scale: 2, null: false
