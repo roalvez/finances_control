0091dc8879117737d6789f6390c236a7a34c2e98
Ronaldo Alves de Andrade <ronaldo.andrade266@gmail.com>
2022-04-22 17:06:22 -0300

    Creates expense model

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Expense, type: :model do
+  describe 'valalidations' do
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:due_at) }
+    it { is_expected.to validate_presence_of(:received_at) }
+  end
+end
