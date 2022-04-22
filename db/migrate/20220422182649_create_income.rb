3cb7c3144927a3357016217e5e17e6315acdefe6
Ronaldo Alves de Andrade <ronaldo.andrade266@gmail.com>
2022-04-22 15:51:33 -0300

    Creates Incomes table

---
+++ db/migrate/20220422182649_create_income.rb
@@ -0,0 +1,12 @@
+class CreateIncome < ActiveRecord::Migration[7.0]
+  def change
+    create_table :incomes do |t|
+      t.decimal :value, null: false, precision: 20, scale: 2
+      t.string :description, null: false, limit: 100
+      t.date :due_at, null: false
+      t.date :received_at, null: false
+
+      t.timestamps
+    end
+  end
+end
